f39e7cc4ca29db9b10594a7a1c18b0156d0f5704
Dysar <david.zingerman@tptlive.ee>
2020-10-05 14:00:49 +0300

    added GetCashIns to pos interface

--- pkg/api/pos/interface.go
+++ pkg/api/pos/interface.go
@@ -6,5 +6,6 @@ type (
 	Manager interface {
 		GetPointsOfSale(ctx context.Context, filters map[string]string) ([]PointOfSale, error)
 		GetDayClosings(ctx context.Context, filters map[string]string) ([]DayClosing, error)
+		GetCashIns(ctx context.Context, filters map[string]string) ([]CashIn, error)
 	}
 )
